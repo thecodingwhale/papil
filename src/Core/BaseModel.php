ae595ffa26c84d3f370cd5b2306efebcbb7ca8ed
Aldren Terante <aldren.terante@salarium.com>
2016-09-27 17:54:31 +0800

    setup core structure files

---
+++ src/Core/BaseModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Papil\Core;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+
+}
---
+++ src/Core/BaseRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Papil\Core;
+
+class BaseRepository
+{
+
+}
