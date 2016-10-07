decc45ab68b0fe8439b0083ebe91e0fad22158c4
Aldren Terante <aldren.terante@gmail.com>
2016-10-08 01:36:48 +0800

    add package for dealing CORS

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Cors\ServiceProvider::class,
+
         Papil\Domain\Task\TaskServiceProvider::class,
     ],
 
